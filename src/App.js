3080fe81794f7efe8a4a3462be4a642838ee23b3
ismailraju <ismailhossainraju74@gmail.com>
2022-08-09 01:29:59 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header'
+import {useState , useEffect} from 'react'
 
 function App() {
+
+  const [inputFields,setInputFields] = useState({});
+
+  const handleFormInput=(event)=>{
+
+    const name = event.target.name;
+    const value = event.target.value;
+
+    setInputFields(fields => ({...fields, [name]:value}));
+
+  }
+
+
+  const handleFormSubmit= (event) => {
+    event.preventDefault();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <Header/>
+        <form onSubmit={handleFormSubmit}>
+          <div className="row">
+            <label>Name</label>
+            <input type="text" className="form-control" name="name" placeholder="Enter Name"
+            value={inputFields.name} onChange={handleFormInput}
+            />
+          </div>
+          <div className="row">
+            <label>Age</label>
+            <input type="number" className="form-control" name="age" placeholder="Enter age"
+            value={inputFields.age} onChange={handleFormInput}
+            />
+          </div>
+          
+          <button className="btn btn-primary" disabled={inputFields.name === '' || inputFields.age === ''} type="submit">Set Api Server</button>
+
+        </form>
+        
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+export default function Header(){
+
+
+    return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+            <ul className="navbar-nav ml-auto">
+                <li className="nav-item">Meet BD</li>
+                <li className="nav-item">Dream Destination</li>
+                <li className="nav-item">Contact Us</li>
+            </ul>
+
+        </div>
+    </nav>
+    );
+}
