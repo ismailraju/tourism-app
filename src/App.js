c88ddd67a8d3614c86795af4fbc43e3de288bb95
ismailraju <ismailhossainraju74@gmail.com>
2022-08-09 18:14:40 +0600

    push 1

--- src/App.js
+++ src/App.js
@@ -1,49 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header'
-import {useState , useEffect} from 'react'
+import LoginForm from './components/LoginForm';
+import { Routes, Route, Link } from "react-router-dom";
+import Home from './components/Home';
+import About from './components/About';
+import Registration from './components/Registration';
+import ForgetPasswordForm from './components/ForgetPasswordForm';
 
 function App() {
 
-  const [inputFields,setInputFields] = useState({});
-
-  const handleFormInput=(event)=>{
-
-    const name = event.target.name;
-    const value = event.target.value;
-
-    setInputFields(fields => ({...fields, [name]:value}));
-
-  }
-
-
-  const handleFormSubmit= (event) => {
-    event.preventDefault();
-  }
 
   return (
     <>
       <div className="App">
-        <Header/>
-        <form onSubmit={handleFormSubmit}>
-          <div className="row">
-            <label>Name</label>
-            <input type="text" className="form-control" name="name" placeholder="Enter Name"
-            value={inputFields.name} onChange={handleFormInput}
-            />
-          </div>
-          <div className="row">
-            <label>Age</label>
-            <input type="number" className="form-control" name="age" placeholder="Enter age"
-            value={inputFields.age} onChange={handleFormInput}
-            />
-          </div>
-          
-          <button className="btn btn-primary" disabled={inputFields.name === '' || inputFields.age === ''} type="submit">Set Api Server</button>
 
-        </form>
+      <Routes>
+        <Route path="/" element={<LoginForm />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/registration" element={<Registration />} />
+        <Route path="/forgetPassword" element={<ForgetPasswordForm />} />
+      </Routes>
         
-      </div>
+        </div>
     </>
   );
 }
